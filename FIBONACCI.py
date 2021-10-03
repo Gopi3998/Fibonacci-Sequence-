3eb7e6bbb65e51de303b8ddc4e23fbaeba383bd4
Gopi3998 <91726200+Gopi3998@users.noreply.github.com>
2021-10-03 01:17:44 -0700

    Add files via upload

---
+++ FIBONACCI.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+x,y=0,1
+while y<50:
+    print(y,end='\t')
+    x,y = y,x+y
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
